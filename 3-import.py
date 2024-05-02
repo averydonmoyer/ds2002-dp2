6282870a35a53e237a1381b348c9e5946e9e0a7c
avery donmoyer <hdj3fw@virginia.edu>
2024-05-02 23:01:25 +0000

    updating code

--- 3-import.py
+++ 3-import.py
@@ -3,46 +3,36 @@ from bson.json_util import dumps
 import os
 import json
 
-
+# connect to MongoDB 
 MONGOPASS = os.getenv('MONGOPASS')
 uri = "mongodb+srv://cluster0.pnxzwgz.mongodb.net/"
-client = MongoClient(uri, username='nmagee', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
+client = MongoClient(uri, username='hdj3fw', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
 
 # define database and collection 
-db = client["hdj3fw"]
+db = client.hdj3fw
+collection = db.dp2_test
 
-db.create_collection("dp2_files") 
+# set up trackers 
+imported = 0 
+orphaned = 0 
+corrupted = 0
 
-collection = db.dp2_files
+directory = "data/" 
 
-
-directory = "data"
-
+# write the loop  
 for filename in os.listdir(directory):
-  with open(os.path.join(directory, filename)) as f:
-    print(f)
-    # do other things with f
-
-def get_files(): 
-    try: 
-        for _ in range(50): 
+     with open(os.path.join(directory, filename)) as f: 
+        try: 
             file_data = json.load(f)
-
-            if isinstance(file_data, list):
-                try:
+            try: 
+                if isinstance(file_data, list):
                     collection.insert_many(file_data)
-                except Exception as e:
-                    print(e, "when importing into Mongo")
+                    print(f, " successfully imported")
+            except Exception as e:
+                print(e, "when importing file")
+                continue 
             else:
-                try:
-                    collection.insert_one(file_data)
-                except Exception as e:
-                    print(e)
-                    continue 
+                collection.insert_one(file_data)
+        except Exception as e:
+            print(e, " when loading file ", f) 
 
-    finally: 
-        pass 
-
-# call the function 
-if __name__ == "__main__": 
-    get_files() 
