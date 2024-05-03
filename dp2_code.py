d151a4568fa25285737a1baf72a2ad23145fd400
avery donmoyer <hdj3fw@virginia.edu>
2024-05-03 02:33:28 +0000

    finishing touches

--- dp2_code.py
+++ dp2_code.py
@@ -4,37 +4,32 @@ import os
 import json
 
 # connect to MongoDB 
+
 MONGOPASS = os.getenv('MONGOPASS')
-uri = “mongodb+srv://hdj3fw:<password>@ds2002.plyczmo.mongodb.net/“
-client = MongoClient(uri, username='hdj3fw', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
+uri = "mongodb+srv://cluster0.pnxzwgz.mongodb.net/"
+client = MongoClient(uri, username='nmagee', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
 
 # define database and collection 
 db = client.hdj3fw
 collection = db.dp2_test
 
+directory = "data" 
+
 # loop over every file for import 
-def import_files(source): 
-    for filename in os.listdir(directory):
-        with open(os.path.join(directory, filename)) as f: 
-            try: 
-                file_data = json.load(f)
-                if isinstance(file_data, list):
-                    try: 
-                        collection.insert_many(file_data)
-                        print(f, " successfully imported")
-                    except Exception as e:
-                        print(e, "when importing file")
-                        continue 
-                else:
-                    collection.insert_one(file_data)
-            except Exception as e:
-                print(e, " when loading file ", f) 
-            finally: 
-                pass 
-
-def add_data(): 
-    directory = "data" 
-    import_files(directory)
-
-if __name__ == "__main__": 
-    add_data() 
+for filename in os.listdir(directory):
+    with open(os.path.join(directory, filename)) as f: 
+        try: 
+            file_data = json.load(f)
+            if isinstance(file_data, list):
+                try: 
+                    collection.insert_many(file_data)
+                    print(f, " successfully imported")
+                except Exception as e:
+                    print(e, "when importing file")
+                    continue 
+            else:
+                collection.insert_one(file_data)
+        except Exception as e:
+            print(e, " when loading file ", f) 
+        finally: 
+            pass 
